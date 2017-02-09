b83394eb3df343e9a54356728ab0097253317556
urshofer <u.hofer@tr51.org>
2017-02-09 12:38:35 +0100

    fixes

--- config/config.local.js
+++ config/config.local.js
@@ -1,16 +1,16 @@
 
 module.exports = {
-  server      = '';       // CouchDB Url
-  port        = 6984;     // CouchDB Port
-  username    = '';       // CouchDB Administrator Name
-  userpass    = '';       // CouchDB Administrator Password
-  api = {
+  server      : '',       // CouchDB Url
+  port        : 6984,     // CouchDB Port
+  username    : '',       // CouchDB Administrator Name
+  userpass    : '',       // CouchDB Administrator Password
+  api : {
     "endpoint" : '',      // Rokfor API Endpoint (Ending on /api/)
     "user"     : '',      // Rokfor API User (needs root level)
     "rwkey"    : '',      // Rokfor R/W Key for JWT Creation
     "rokey"    : '',      // Rokfor R/O Key
     "chapter"  : 1,       // Rokfor Chapter ID for Data Syncing
     "template" : 1        // Rokfor Template ID for Data Syncing
-  };
-  pollport    = 5050;     // Listener Port for Route Hook Callbacks
+  },
+  pollport    : 5050     // Listener Port for Route Hook Callbacks
 }
